8e830da419d470c2f83546f1276c54a929736953
WahajAhmedIqbal <wahaj465@gmail.com>
2020-12-09 22:49:52 +0500

    login form

--- src/containers/login/LoginContainer.js
+++ src/containers/login/LoginContainer.js
@@ -1,9 +1,26 @@
+import { Container, Grid } from "@material-ui/core";
 import React from "react";
 import "./LoginContainer.scss";
+import LeftImage from "../../images/signin-image.jpg";
 
 const LoginContainer = () => {
   return (
-    
+    <React.Fragment>
+      <Container
+        className="main-container"
+        maxWidth="xl"
+        style={{ height: "100vh", backgroundColor: "pink" }}
+      >
+        <Grid container>
+          <Grid xs={6} sm={6} md={6} item className="left-side">
+            <img src={LeftImage} alt="" />
+          </Grid>
+          <Grid xs={6} sm={6} md={6} item className="right-side">
+            <form className="form">this is form</form>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 };
 
