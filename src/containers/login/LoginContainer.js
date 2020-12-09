da5f05013a38fe33fd7b16e86e1bd1ac1d47cf38
WahajAhmedIqbal <wahaj465@gmail.com>
2020-12-10 00:08:55 +0500

    text feild add

--- src/containers/login/LoginContainer.js
+++ src/containers/login/LoginContainer.js
@@ -1,22 +1,46 @@
-import { Container, Grid } from "@material-ui/core";
 import React from "react";
 import "./LoginContainer.scss";
 import LeftImage from "../../images/signin-image.jpg";
+import { Container, Grid } from "@material-ui/core";
 
 const LoginContainer = () => {
   return (
     <React.Fragment>
       <Container
-        className="main-container"
         maxWidth="xl"
-        style={{ height: "100vh", backgroundColor: "pink" }}
+        style={{ backgroundColor: "pink", height: "100vh" }}
       >
-        <Grid container>
-          <Grid xs={6} sm={6} md={6} item className="left-side">
+        <Grid
+          container
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <Grid
+            item
+            style={{
+              backgroundColor: "white",
+              width: "50%",
+              height: "60%",
+              textAlign: "center",
+            }}
+          >
             <img src={LeftImage} alt="" />
+            <h1>Create an account</h1>
           </Grid>
-          <Grid xs={6} sm={6} md={6} item className="right-side">
-            <form className="form">this is form</form>
+          <Grid
+            item
+            style={{
+              backgroundColor: "yellow",
+              height: "60%",
+              width: "50%",
+              textAlign: "center",
+              justifyItems: "center",
+            }}
+          >
+            <h1>Sign up</h1>
           </Grid>
         </Grid>
       </Container>
